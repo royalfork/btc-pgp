ca1a3723fee782efbf7f782df2d3b1f60fff80eb
Royal Fork <rf@royalforkblog.com>
2014-12-12 12:14:51 +0100

    Setup works well

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
 var app = angular.module('app', ['ngCookies']);
 
-app.controller('BtcPgp', function($scope, $cookies, $q, $timeout) {
+Number.prototype.toSatoshi = function() {
+    if (isNaN(this)) return NaN;
+    if (this === 0) return 0;
+    var str = this.toString();
+    var sign = (str.indexOf('-') === 0) ? "-" : "";
+    str = str.replace(/^-/, '');
+    if (str.indexOf('e') >=0) {
+        return parseInt(sign + str.replace(".", "").replace(/e-8/, "").replace(/e-7/, "0"), 10);
+    } else {
+        if (!(/\./).test(str)) str += ".0";
+        var parts = str.split(".");
+        str = parts[0] + "." + parts[1].slice(0,8);
+        while (!(/\.[0-9]{8}/).test(str)) {
+            str += "0";
+        }
+        return parseInt(sign + str.replace(".", "").replace(/^0+/, ""), 10);
+    }
+};
+
+Number.prototype.toBitcoinString = function() {
+    if (isNaN(this)) return NaN;
+    if (this === 0) return 0;
+    var str = parseInt(this, 10).toString();
+    var sign = (str.indexOf('-') === 0) ? "-" : "";
+    str = str.replace(/^-/, '');
+    var lengthTester = (/[0-9]{8}/);
+    while (!lengthTester.test(str)) {
+        str = "0" + str;
+    }
+    str = str.slice(0, str.length - 8) + "." + str.slice(str.length - 8);
+    if (str[0] === '.') str = '0' + str;
+    return sign + str;
+};
+
+Number.prototype.toBitcoin = function() {
+    return parseFloat(this.toBitcoinString());
+};
+
+
+app.controller('BtcPgp', function($scope, $cookies, $q, $http, $timeout) {
 
   $scope.bitcoin = bitcoin;
 
@@ -13,18 +52,22 @@ app.controller('BtcPgp', function($scope, $cookies, $q, $timeout) {
     $scope.key = bitcoin.ECKey.fromWIF($scope.wif);
   }
 
-  $scope.spender = 50;
-
   $scope.createRandom = function() {
     // clear passphrase
     if ($scope.passphrase) {
       $scope.passphrase = "";
     }
     $scope.key = bitcoin.ECKey.makeRandom();
+    console.log($scope.addr($scope.key).length);
   };
 
+  $scope.addr =  function (key) {
+    if (key) {
+      return key.pub.getAddress(bitcoin.networks.testnet).toString(); 
+    }
+  }
+
   $scope.$watch('passphrase', function(val) {
-    console.log("Passphrase changed to: " + val);
     if (val) {
       var hash = bitcoin.crypto.sha256(val);
       $scope.key = bitcoin.ECKey.fromUint8Arr(true, hash);
@@ -47,86 +90,114 @@ app.controller('BtcPgp', function($scope, $cookies, $q, $timeout) {
   };
 
   $scope.$watch('key', function(val) {
-    console.log("KEY CHANGED TO: " + val);
     if (val) {
       $scope.wif = $scope.key.toWIF();
     }
   });
 
   $scope.save = function() {
-    $cookies.passphrase = $scope.passphrase;
+    if ($scope.passphrase) {
+      $cookies.passphrase = $scope.passphrase;
+    }
     $cookies.wif = $scope.wif;
-    alert("Your bitcoin keys have been saved as cookies.");
+    alert("WIF " + $scope.wif + " has been saved as a cookie.");
   }
 
-  $scope.range2Btc = function(range) {
-    return parseFloat((range * .001).toPrecision(2));   
+  // parse verose raw transaction from bitcoind and return utxo index of addr
+  function getUtxoIndex (txn, addr) {
+    for (var i = 0, l = txn.vout.length; i < l; i ++) {
+      var v = txn.vout[i];
+      if (v.scriptPubKey.addresses[0] === addr) {
+        return v.n; 
+      }
+    }
   }
 
   $scope.fund = function(amount, addr) {
-    //freeze address input fields
-    //var satoshis = parseInt((amount * Math.pow(10, 8)).toPrecision(3));
-
-    $scope.fundStatus = "Requesting BTC...";
-    $scope.progress = 10;
-    // XXX this needs to use promise
-    //startWebsocket(addr, function(resp) {
-      //$scope.fundStatus = "Transaction complete";
-      //debugger;
-    //});
-
-    // XXX this needs to use angular http service
-    //$.post("http://faucet.xeno-genesis.com/request", { address: addr, amount: amount }, function() {
-      //$scope.fundStatus = "Request sent successfully.  Listening on blockchain (usually takes around 5 seconds)...";
-      //console.log(arguments);
-    //});
-
-    testWebsocket(addr).then(function(resp) {
-      //$scope.fundStatus = "Complete";
-      // we have prev txn, set utxo
-      console.log(resp);
-      $scope.progress = 90;
-    });
-  }
-
-  function testWebsocket(address) {
-    var mock = '{"transaction_hash":"747b64808595329ef48eb75924e2d9f458215e3b9ffad80721a5dd756a558a59","output_index":0,"value":2900000,"addresses":["mvDbXbTSUwi958YjsH6oY1YMd6udps37fV"],"script":"OP_DUP OP_HASH160 a1417af9cebb676cf858f9c603a258ed5ec6208c OP_EQUALVERIFY OP_CHECKSIG","script_hex":"76a914a1417af9cebb676cf858f9c603a258ed5ec6208c88ac","script_type":"pubkeyhash","required_signatures":1,"spent":false}';
-    mock = JSON.parse(mock);
     return $q(function(resolve, reject) {
-      $timeout(function() {
-        resolve(mock);
-      }, 500);
-    })
-  }
+      $http.post("http://localhost:4444", { address: addr, amount: amount.toSatoshi(), verbose: true }).then(function (resp) {
+        $scope.utxo = resp.data.txn; 
+        resolve();
+      }, function(error) {
+        alert("There was an error funding your address.  Please refresh and try again. If the problem persists, please email rf@royalforkblog.com");
+        resolve();
+      });
+    });
+  };
 
-  function startWebsocket(address, cb) {
-    var conn = new WebSocket("wss://ws.chain.com/v2/notifications");
-    conn.onopen = function (ev) {
-      var req = {type: "new-transaction", block_chain: "testnet3"};
-      conn.send(JSON.stringify(req));
-    };
-    conn.onmessage = function (ev) {
-      var x = JSON.parse(ev.data);
-      console.log(x);
-      if (x.payload.transaction.outputs) {
-        for (var i = 0, l = x.payload.transaction.outputs.length; i < l; i ++) {
-          var v = x.payload.transaction.outputs[i];
-          if (v.addresses) {
-            for (var j = 0, k = v.addresses.length; j < k; j ++) {
-              var a = v.addresses[j];
-              if (a === address) {
-                console.log("Transaction found: " + v.transaction_hash);
-                conn.close();
-                cb(v);
-              }
-            }
-          }
-        }
+  $scope.broadcast = function(key, utxo, message) {
+    return $q(function(resolve, reject) {
+      if (message.length > 40) {
+        resolve();
+        return alert("Message is too long.");
       }
-    };
+
+      // create message
+      var addr = key.pub.getAddress(bitcoin.networks.testnet).toString();
+      var idx = getUtxoIndex(utxo, addr);
+      // add input
+      var tx = new bitcoin.TransactionBuilder();
+      tx.addInput(utxo.txid, idx);
+
+      // create op_return script
+      var script = bitcoin.Script.fromASM("OP_RETURN " + a2hex("mx!"+message));
+      tx.addOutput(script, 0);
+
+      // add change
+      tx.addOutput(addr, utxo.vout[idx].value.toSatoshi() - 1000);
+
+      tx.sign(0, key);
+      tx = tx.build();
+
+      // broadcast transaction across network
+      $http.post("http://localhost:4444/sendraw", { hex: tx.toHex() }).then(function(resp) {
+        $scope.fundTxn = resp.data;
+        resolve();
+      }, function(error) {
+        alert("There was an error funding your address.  Please refresh and try again. If the problem persists, please email rf@royalforkblog.com");
+        console.log(error);
+        resolve();
+      });
+    })
+  };
+
+  function hex2a(hexx) {
+    var hex = hexx.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
   }
 
-
+  function a2hex(str) {
+    var arr = [];
+    for (var i = 0, l = str.length; i < l; i ++) {
+      var hex = Number(str.charCodeAt(i)).toString(16);
+      arr.push(hex);
+    }
+    return arr.join('');
+  }
 
 });
 
+app.directive('statusButton', function($q) {
+    return {
+      restrict: 'AEC',
+      scope: {click:'&?'},
+      bindToController: true,
+      controllerAs: 'ctrl',
+      template: "<div class=\"ctrl.class\" ng-class=\"{ 'loading': ctrl.state==='loading', 'complete': ctrl.state==='complete' }\"><input class=\"btn\" type=\"button\" ng-value=\"ctrl.value\" ng-click=\"ctrl.action()\"></div>",
+      transclude: true,
+      controller: function($scope, $element, $attrs) {
+        var ctrl = this;
+        ctrl.value = $attrs.value;
+        var fn = ctrl.click || function() { return $q.when('done')};
+        this.action = function() {
+          ctrl.state = 'loading';
+          fn().then(function(res) {
+            ctrl.state = 'complete';
+          });
+        }
+      }
+    }
+  });
